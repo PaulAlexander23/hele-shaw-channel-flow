44841246cbe67218cb67732b5aee11bc11217719
Paul Alexander <paulalexander23@live.co.uk>
2021-10-18 11:02:26 +0100

    Use 2d Possion with flux bc demo and adapt
    Copy demo into folder.
    Split out problem_parameters and hele_shaw_channel_problem.
    Create validata and validate.sh.

---
+++ problem_parameters.h
@@ -0,0 +1,46 @@
+#pragma once
+
+#include "generic.h"
+
+using namespace oomph;
+
+//===== start_of_namespace=============================================
+/// Namespace for exact solution for Poisson equation with "sharp step" 
+//=====================================================================
+namespace TanhSolnForPoisson
+{
+
+ /// Parameter for steepness of "step"
+ double Alpha=1.0;
+
+ /// Parameter for angle Phi of "step"
+ double TanPhi=0.0;
+
+ /// Exact solution as a Vector
+ void get_exact_u(const Vector<double>& x, Vector<double>& u)
+ {
+  u[0]=tanh(1.0-Alpha*(TanPhi*x[0]-x[1]));
+ }
+
+ /// Source function required to make the solution above an exact solution 
+ void source_function(const Vector<double>& x, double& source)
+ {
+  source = 2.0*tanh(-1.0+Alpha*(TanPhi*x[0]-x[1]))*
+   (1.0-pow(tanh(-1.0+Alpha*(TanPhi*x[0]-x[1])),2.0))*
+   Alpha*Alpha*TanPhi*TanPhi+2.0*tanh(-1.0+Alpha*(TanPhi*x[0]-x[1]))*
+   (1.0-pow(tanh(-1.0+Alpha*(TanPhi*x[0]-x[1])),2.0))*Alpha*Alpha;
+ }
+
+ /// Flux required by the exact solution on a boundary on which x is fixed
+ void prescribed_flux_on_fixed_x_boundary(const Vector<double>& x, 
+                                          double& flux)
+ {
+  //The outer unit normal to the boundary is (1,0)
+  double N[2] = {1.0, 0.0};
+  //The flux in terms of the normal is
+  flux = 
+   -(1.0-pow(tanh(-1.0+Alpha*(TanPhi*x[0]-x[1])),2.0))*Alpha*TanPhi*N[0]+(
+   1.0-pow(tanh(-1.0+Alpha*(TanPhi*x[0]-x[1])),2.0))*Alpha*N[1];
+ }
+ 
+} // end of namespace
